c01d4a01f5363a9d9ba7a89ca17a9f08cc602f97
brutus1231 <ztrzaska2@gmail.com>
2019-06-02 13:16:20 +0200

    Initial commit

---
+++ src/main/java/pl/sda/springbootweb8/SpringbootWeb8Application.java
@@ -0,0 +1,13 @@
+package pl.sda.springbootweb8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWeb8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWeb8Application.class, args);
+	}
+
+}
